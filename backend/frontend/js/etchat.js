6c3af03485f3422e5aae1ca95cd6dd68b0495f5c
satonaka-chie9 <kurokisyun9@gmail.com>
2026-03-05 13:04:37 +0900

    試験的にFabric.jsを導入

--- backend/frontend/js/etchat.js
+++ backend/frontend/js/etchat.js
@@ -1,61 +1,14 @@
-const socket = io();
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = new fabric.Canvas('c', { isDrawingMode: true });
+canvas.freeDrawingBrush.width = 3;
+canvas.freeDrawingBrush.color = "black";
 
-let drawing = false;
-let lastX = 0;
-let lastY = 0;
-
-// 線の設定
-ctx.lineWidth = 2;
-ctx.lineCap = "round";
-ctx.lineJoin = "round";
-ctx.strokeStyle = "black";
-
-canvas.addEventListener("mousedown", (e) => {
-  drawing = true;
-  const rect = canvas.getBoundingClientRect();
-  lastX = e.clientX - rect.left;
-  lastY = e.clientY - rect.top;
+canvas.on("path:created", function(opt) {
+  const data = opt.path.toObject();
+  socket.emit("draw", data);
 });
 
-canvas.addEventListener("mouseup", () => {
-  drawing = false;
-  socket.emit("drawEnd");
-});
-
-canvas.addEventListener("mouseout", () => {
-  drawing = false;
-  socket.emit("drawEnd");
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (!drawing) return;
-
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  // ローカルで描画
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-
-  // 他のクライアントに送信
-  socket.emit("draw", { x0: lastX, y0: lastY, x1: x, y1: y });
-
-  lastX = x;
-  lastY = y;
-});
-
-socket.on("draw", (data) => {
-  ctx.beginPath();
-  ctx.moveTo(data.x0, data.y0);
-  ctx.lineTo(data.x1, data.y1);
-  ctx.stroke();
-});
-
-socket.on("drawEnd", () => {
-  // リモートクライアントの描画終了
+socket.on("draw", function(data) {
+  fabric.Path.fromObject(data, function(path) {
+    canvas.add(path);
+  });
 });
