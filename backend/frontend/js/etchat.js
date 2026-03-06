f62457ff5c53aa6761a943da9696d02e14669d10
satonaka-chie9 <kurokisyun9@gmail.com>
2026-03-06 13:16:44 +0900

    絵茶においてアクセスカウンタが機能しない問題を修正

--- backend/frontend/js/etchat.js
+++ backend/frontend/js/etchat.js
@@ -166,9 +166,10 @@ socket.on("chat", (data) => {
 /* ===== アクセスカウンタ ===== */
 
 async function updateCounter() {
+  await fetch("/api/counter/increment", { method: "POST" });
   const res = await fetch("/api/counter");
   const data = await res.json();
   document.getElementById("counter").innerText =
-    String(data.count).padStart(4, "0");
+    String(data.count).padStart(6, "0");
 }
 updateCounter();
